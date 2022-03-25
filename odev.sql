29a63ade44b683b7a6c0864c595651f56b38a024
m.engin <engindads@gmail.com>
2022-03-25 23:38:55 +0300

    update

--- odev.sql
+++ odev.sql
@@ -5,6 +5,6 @@
 SELECT title, description FROM film;
 
 -- 2. film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
-
+SELECT
 
 -- 3. film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
